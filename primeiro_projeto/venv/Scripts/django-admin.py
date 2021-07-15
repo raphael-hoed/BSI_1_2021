67b0bedc4f17c4f83e0be6c68e7251005e5b82a8
raphael-hoed <raphael.hoed@gmail.com>
2021-07-15 15:18:58 -0300

    Primeira versão projeto BSI com página index e arquivos estáticos

---
+++ primeiro_projeto/venv/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!d:\ifnmg campus januária\disciplinas 2021_1\desenvolvimento web avançado\aulas\templates backend - básico\exemplos_django\primeiro_projeto\venv\scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
